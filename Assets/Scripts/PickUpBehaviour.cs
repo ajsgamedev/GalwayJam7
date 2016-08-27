71b2c5184f4f11f13ff21b92105127e7f3b36c4f
Alan S <alstew13@hotmail.com>
2016-08-27 14:18:23 +0100

    PickUp objects

---
+++ Assets/Scripts/PickUpBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PickUpBehaviour : MonoBehaviour
+{
+
+	public int score;
+	public int healthDamage;
+	public int totalLife = 100;
+	public int liveLeft = 100;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			
+			ScoreManager.score += score;
+			HandlePickUp ();
+			Destroy (this.gameObject);
+
+		}
+	}
+
+	void HandlePickUp()
+	{
+		liveLeft -= healthDamage;
+		GameObject.FindGameObjectWithTag ("Health").GetComponent<Image> ().fillAmount = 
+			(float)liveLeft / totalLife;
+		
+	}
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -5,7 +5,7 @@ using System;
 
 public class ScoreManager : MonoBehaviour
 {
-	public static double score;        // The player's score.
+	public static int score;        // The player's score.
 
 	Text text;                      // Reference to the Text component.
 
@@ -15,7 +15,7 @@ public class ScoreManager : MonoBehaviour
 		text = GetComponent <Text> ();
 
 		// Reset the score.
-		score = 0.0;
+		score = 0;
 	}
 
 
@@ -24,9 +24,8 @@ public class ScoreManager : MonoBehaviour
 		// Set the displayed text to be the word "Score" followed by the score value.
 		if (score < 0)
 		{
-			score = 0.0;
+			score = 0;
 		}
-		score = Math.Round (score, 2);
-		text.text = "Votes: " + score + "M";
+		text.text = "Score: " + score;
 	}
 }
